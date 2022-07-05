333015ec9dff2c8848fe8af6ef74fc4c49e73bab
Divyank <dubeydivyank@gmail.com>
2022-07-05 10:45:37 +0530

    login using email & password

--- src/components/modal/Login.jsx
+++ src/components/modal/Login.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 import { useState } from "react";
 import "./Login.css";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
 const Login = ({ setLoginPage }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="login-form-container">
-      <input type="email" placeholder="Email Address" />
-      <input type="password" placeholder="Password" />
-      <button type="submit">Login</button>
+      <input
+        type="email"
+        placeholder="Email Address"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button type="submit" onClick={loginHandler}>
+        Login
+      </button>
       <div>Sign in with Google</div>
       <div
         style={{ cursor: "pointer" }}
@@ -19,6 +47,7 @@ const Login = ({ setLoginPage }) => {
       >
         Don't have an Account? SignUp
       </div>
+      {error && <div>{error}</div>}
     </div>
   );
 };
